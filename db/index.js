ae737caa78ec321beb9b83c1578ca8a075463217
kuhakuni <afif.haidar@student.ub.ac.id>
2023-06-02 10:05:56 +0700

    add more tables to db

--- db/index.js
+++ db/index.js
@@ -60,9 +60,18 @@ const User = sequelize.define("users", {
 		type: DataTypes.BOOLEAN,
 		defaultValue: false,
 	},
+});
+
+const Otp = sequelize.define("otps", {
+	id: {
+		type: DataTypes.UUID,
+		defaultValue: UUIDV4,
+		primaryKey: true,
+		allowNull: false,
+	},
 	otp: {
-		type: DataTypes.STRING(6),
-		allowNull: true,
+		type: DataTypes.STRING(100),
+		allowNull: false,
 	},
 });
 
@@ -116,23 +125,6 @@ const Franchise = sequelize.define("franchises", {
 	},
 });
 
-const Review = sequelize.define("reviews", {
-	id: {
-		type: DataTypes.UUID,
-		defaultValue: UUIDV4,
-		primaryKey: true,
-		allowNull: false,
-	},
-	rating: {
-		type: DataTypes.INTEGER,
-		allowNull: false,
-	},
-	review: {
-		type: DataTypes.STRING(100),
-		allowNull: false,
-	},
-});
-
 const FranchisePackage = sequelize.define("franchise_packages", {
 	id: {
 		type: DataTypes.UUID,
@@ -148,6 +140,113 @@ const FranchisePackage = sequelize.define("franchise_packages", {
 		type: DataTypes.INTEGER,
 		allowNull: false,
 	},
+});
+
+const Agreement = sequelize.define("agreements", {
+	id: {
+		type: DataTypes.UUID,
+		defaultValue: UUIDV4,
+		primaryKey: true,
+		allowNull: false,
+	},
+	franchise_id: {
+		type: DataTypes.UUID,
+		allowNull: false,
+	},
+	user_id: {
+		type: DataTypes.UUID,
+		allowNull: false,
+	},
+	franchise_package_id: {
+		type: DataTypes.UUID,
+		allowNull: false,
+	},
+	status: {
+		type: DataTypes.STRING(100),
+		allowNull: false,
+	},
+	users_location: {
+		type: DataTypes.STRING(100),
+		allowNull: false,
+	},
+	users_photo_location: {
+		type: DataTypes.STRING(100),
+		allowNull: false,
+	},
+	agreement_document: {
+		type: DataTypes.STRING(100),
+		allowNull: false,
+	},
+});
+
+const ForumDiscussion = sequelize.define("forum_discussions", {
+	id: {
+		type: DataTypes.UUID,
+		defaultValue: UUIDV4,
+		primaryKey: true,
+		allowNull: false,
+	},
+	user_id: {
+		type: DataTypes.UUID,
+		allowNull: false,
+	},
+	discussion: {
+		type: DataTypes.STRING(100),
+		allowNull: false,
+	},
+});
+
+const ForumReply = sequelize.define("forum_replies", {
+	id: {
+		type: DataTypes.UUID,
+		defaultValue: UUIDV4,
+		primaryKey: true,
+		allowNull: false,
+	},
+	user_id: {
+		type: DataTypes.UUID,
+	},
+	franchise_id: {
+		type: DataTypes.UUID,
+	},
+	forum_discussion_id: {
+		type: DataTypes.UUID,
+		allowNull: false,
+	},
+	reply: {
+		type: DataTypes.STRING(100),
+		allowNull: false,
+	},
+});
+
+User.hasMany(Agreement, {
+	foreignKey: "user_id",
+	as: "agreements",
+});
+
+Agreement.belongsTo(User, {
+	foreignKey: "user_id",
+	as: "user",
+});
+
+Franchise.hasMany(Agreement, {
+	foreignKey: "franchise_id",
+	as: "agreements",
+});
+
+Agreement.belongsTo(Franchise, {
+	foreignKey: "franchise_id",
+	as: "franchise",
+});
+
+FranchisePackage.hasMany(Agreement, {
+	foreignKey: "franchise_package_id",
+	as: "agreements",
+});
+
+Agreement.belongsTo(FranchisePackage, {
+	foreignKey: "franchise_package_id",
+	as: "franchise_package",
 });
 
 User.hasMany(Franchise, {
@@ -170,26 +269,6 @@ FranchiseCategory.hasMany(Franchise, {
 	as: "franchises",
 });
 
-Franchise.hasMany(Review, {
-	foreignKey: "franchise_id",
-	as: "reviews",
-});
-
-Review.belongsTo(Franchise, {
-	foreignKey: "franchise_id",
-	as: "franchise",
-});
-
-User.hasMany(Review, {
-	foreignKey: "user_id",
-	as: "reviews",
-});
-
-Review.belongsTo(User, {
-	foreignKey: "user_id",
-	as: "user",
-});
-
 Franchise.hasMany(FranchisePackage, {
 	foreignKey: "franchise_id",
 	as: "franchise_packages",
@@ -200,4 +279,53 @@ FranchisePackage.belongsTo(Franchise, {
 	as: "franchise",
 });
 
-module.exports = { User, Franchise, sequelize };
+ForumDiscussion.hasMany(ForumReply, {
+	foreignKey: "forum_discussion_id",
+	as: "forum_replies",
+});
+
+ForumReply.belongsTo(ForumDiscussion, {
+	foreignKey: "forum_discussion_id",
+	as: "forum_discussion",
+});
+
+User.hasMany(ForumDiscussion, {
+	foreignKey: "user_id",
+	as: "forum_discussions",
+});
+
+ForumDiscussion.belongsTo(User, {
+	foreignKey: "user_id",
+	as: "user",
+});
+
+User.hasMany(ForumReply, {
+	foreignKey: "user_id",
+	as: "forum_replies",
+});
+
+ForumReply.belongsTo(User, {
+	foreignKey: "user_id",
+	as: "user",
+});
+
+Franchise.hasMany(ForumReply, {
+	foreignKey: "franchise_id",
+	as: "forum_replies",
+});
+
+ForumReply.belongsTo(Franchise, {
+	foreignKey: "franchise_id",
+	as: "franchise",
+});
+
+module.exports = {
+	User,
+	Franchise,
+	FranchiseCategory,
+	FranchisePackage,
+	Agreement,
+	ForumDiscussion,
+	ForumReply,
+	sequelize,
+};
