f122e63f57858d99801742feba32c3c6a20ba5f3
kuhakuni <afif.haidar@student.ub.ac.id>
2023-06-12 21:13:13 +0700

    add agreement feature

--- controllers/agreement.controller.js
+++ controllers/agreement.controller.js
@@ -1,50 +1,84 @@
 const express = require("express");
 const agreementRouter = express.Router();
 const Agreement = require("../models/agreement.model");
+const uploadFile = require("../helpers/fileUpload");
+const multer = require("multer");
+const upload = multer({
+	storage: multer.memoryStorage(),
+	limits: {
+		fileSize: 1024 * 1024 * 5,
+	},
+});
 const jwt = require("../middleware/jwt");
 const joi = require("joi");
 
 agreementRouter.get("/", async (_req, res) => {
-  const response = await Agreement.getAllAgreements();
-  res.status(response.status).send(response);
+	const response = await Agreement.getAllAgreements();
+	res.status(response.statusCode).send(response);
 });
 
 agreementRouter.get("/:id", async (req, res) => {
-  const response = await Agreement.getAgreementById(req.params.id);
-  res.status(response.status).send(response);
+	const response = await Agreement.getAgreementById(req.params.id);
+	res.status(response.statusCode).send(response);
 });
 
-agreementRouter.post("/", jwt.verifyToken, async (req, res) => {
-  const schema = joi.object({
-    // Define your validation schema for the agreement data here
-  });
+agreementRouter.post(
+	"/",
+	jwt.verifyToken,
+	upload.fields([
+		{ name: "location_photo", maxCount: 1 },
+		{ name: "agreement_document", maxCount: 1 },
+	]),
+	async (req, res) => {
+		const schema = joi.object({
+			franchise_id: joi.string().required(),
+			franchise_package_id: joi.string().required(),
+			users_location: joi.string().required(),
+		});
 
-  const { error } = schema.validate(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
+		const { error } = schema.validate(req.body);
+		if (error) {
+			return res.status(400).send(error.details[0].message);
+		}
+		//handle file upload for non required file
+		if (req.files["location_photo"]) {
+			const location_photo = await uploadFile(
+				req.files["location_photo"][0],
+				"location_photo"
+			);
+			req.body.users_photo_location = location_photo;
+		}
+		if (req.files["agreement_document"]) {
+			const agreement_document = await uploadFile(
+				req.files["agreement_document"][0],
+				"agreement_document"
+			);
+			req.body.agreement_document = agreement_document;
+		}
 
-  const response = await Agreement.createAgreement(req.body);
-  res.status(response.status).send(response);
-});
+		const response = await Agreement.createAgreement({
+			...req.body,
+			user_id: req.userId,
+		});
+		res.status(response.statusCode).send(response);
+	}
+);
 
 agreementRouter.put("/:id", jwt.verifyToken, async (req, res) => {
-  const schema = joi.object({
-    // Define your validation schema for the updated agreement data here
-  });
+	const schema = joi.object({});
 
-  const { error } = schema.validate(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
+	const { error } = schema.validate(req.body);
+	if (error) {
+		return res.status(400).send(error.details[0].message);
+	}
 
-  const response = await Agreement.updateAgreement(req.params.id, req.body);
-  res.status(response.status).send(response);
+	const response = await Agreement.updateAgreement(req.params.id, req.body);
+	res.status(response.statusCode).send(response);
 });
 
 agreementRouter.delete("/:id", jwt.verifyToken, async (req, res) => {
-  const response = await Agreement.deleteAgreement(req.params.id);
-  res.status(response.status).send(response);
+	const response = await Agreement.deleteAgreement(req.params.id);
+	res.status(response.statusCode).send(response);
 });
 
 module.exports = agreementRouter;
--- models/agreement.model.js
+++ models/agreement.model.js
@@ -1,109 +1,122 @@
 const Agreement = require("../db").Agreement;
 
 const getAllAgreements = async () => {
-  try {
-    const agreements = await Agreement.findAll();
-    return {
-      status: 200,
-      message: "Agreements retrieved!",
-      data: agreements,
-    };
-  } catch (err) {
-    return {
-      status: 500,
-      message: err.message,
-    };
-  }
+	try {
+		const agreements = await Agreement.findAll();
+		return {
+			status: true,
+			statusCode: 200,
+			message: "Agreements retrieved!",
+			data: agreements,
+		};
+	} catch (err) {
+		return {
+			status: false,
+			statusCode: 500,
+			message: err.message,
+		};
+	}
 };
 
 const getAgreementById = async (id) => {
-  try {
-    const agreement = await Agreement.findByPk(id);
-    if (agreement) {
-      return {
-        status: 200,
-        message: "Agreement retrieved!",
-        data: agreement,
-      };
-    }
-    return {
-      status: 404,
-      message: "Agreement not found!",
-    };
-  } catch (err) {
-    return {
-      status: 500,
-      message: err.message,
-    };
-  }
+	try {
+		const agreement = await Agreement.findByPk(id);
+		if (agreement) {
+			return {
+				status: true,
+				statusCode: 200,
+				message: "Agreement retrieved!",
+				data: agreement,
+			};
+		}
+		return {
+			status: false,
+			statusCode: 404,
+			message: "Agreement not found!",
+		};
+	} catch (err) {
+		return {
+			status: false,
+			statusCode: 500,
+			message: err.message,
+		};
+	}
 };
 
 const createAgreement = async (data) => {
-  try {
-    const agreement = await Agreement.create(data);
+	try {
+		const agreement = await Agreement.create(data);
 
-    return {
-      status: 200,
-      message: "Agreement created!",
-      data: agreement,
-    };
-  } catch (err) {
-    return {
-      status: 500,
-      message: err.message,
-    };
-  }
+		return {
+			status: true,
+			statusCode: 200,
+			message: "Agreement created!",
+			data: agreement,
+		};
+	} catch (err) {
+		return {
+			status: false,
+			statusCode: 500,
+			message: err.message,
+		};
+	}
 };
 
 const updateAgreement = async (id, data) => {
-  try {
-    const agreement = await Agreement.findByPk(id);
-    if (agreement) {
-      const updatedAgreement = await agreement.update(data);
-      return {
-        status: 200,
-        message: "Agreement updated!",
-        data: updatedAgreement,
-      };
-    }
-    return {
-      status: 404,
-      message: "Agreement not found!",
-    };
-  } catch (err) {
-    return {
-      status: 500,
-      message: err.message,
-    };
-  }
+	try {
+		const agreement = await Agreement.findByPk(id);
+		if (agreement) {
+			const updatedAgreement = await agreement.update(data);
+			return {
+				status: true,
+				statusCode: 200,
+				message: "Agreement updated!",
+				data: updatedAgreement,
+			};
+		}
+		return {
+			status: false,
+			statusCode: 404,
+			message: "Agreement not found!",
+		};
+	} catch (err) {
+		return {
+			status: false,
+			statusCode: 500,
+			message: err.message,
+		};
+	}
 };
 
 const deleteAgreement = async (id) => {
-  try {
-    const agreement = await Agreement.findByPk(id);
-    if (agreement) {
-      await agreement.destroy();
-      return {
-        status: 200,
-        message: "Agreement deleted!",
-      };
-    }
-    return {
-      status: 404,
-      message: "Agreement not found!",
-    };
-  } catch (err) {
-    return {
-      status: 500,
-      message: err.message,
-    };
-  }
+	try {
+		const agreement = await Agreement.findByPk(id);
+		if (agreement) {
+			await agreement.destroy();
+			return {
+				status: true,
+				statusCode: 200,
+				message: "Agreement deleted!",
+			};
+		}
+		return {
+			status: false,
+			statusCode: 404,
+			message: "Agreement not found!",
+		};
+	} catch (err) {
+		return {
+			status: false,
+			statusCode: 500,
+			message: err.message,
+		};
+	}
 };
 
 module.exports = {
-  getAllAgreements,
-  getAgreementById,
-  createAgreement,
-  updateAgreement,
-  deleteAgreement,
+	getAllAgreements,
+	getAgreementById,
+	createAgreement,
+	updateAgreement,
+	deleteAgreement,
 };
